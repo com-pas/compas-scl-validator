56dcd55b6a19b946a8886b8e8678f69118a5a9a7
Flurb <private@tjalma.com>
2022-03-28 11:44:36 +0200

    Refactoring
    Signed-off-by: Flurb <private@tjalma.com>

--- validator/src/main/java/org/lfenergy/compas/scl/validator/xsd/XSDValidator.java
+++ validator/src/main/java/org/lfenergy/compas/scl/validator/xsd/XSDValidator.java
@@ -9,6 +9,8 @@ import java.io.StringReader;
 import java.util.ArrayList;
 
 import javax.xml.XMLConstants;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.transform.Source;
 import javax.xml.transform.sax.SAXSource;
 import javax.xml.transform.stream.StreamSource;
@@ -16,13 +18,18 @@ import javax.xml.validation.Schema;
 import javax.xml.validation.SchemaFactory;
 import javax.xml.validation.Validator;
 
+import org.lfenergy.compas.scl.validator.exception.SclValidatorException;
 import org.lfenergy.compas.scl.validator.model.ValidationError;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.w3c.dom.Document;
+import org.w3c.dom.NamedNodeMap;
 import org.xml.sax.ErrorHandler;
 import org.xml.sax.InputSource;
 import org.xml.sax.SAXException;
 import org.xml.sax.SAXParseException;
+
+import static org.lfenergy.compas.scl.validator.exception.SclValidatorErrorCode.CONVERTING_SCL_FILE_ERROR_CODE;
 
 public class XSDValidator {
     private static final Logger LOGGER = LoggerFactory.getLogger(XSDValidator.class);
@@ -35,10 +42,10 @@ public class XSDValidator {
         this.errorList = errorList;
     }
 
-    public void prepare(String xsdFile) {
+    private void prepare(String sclData) {
         SchemaFactory factory = SchemaFactory.newInstance( XMLConstants.W3C_XML_SCHEMA_NS_URI );
 
-        Source schemaFile = new StreamSource(new File(xsdFile));
+        Source schemaFile = new StreamSource(new File(getXsdPath(sclData)));
         Schema schema;
         try {
             schema = factory.newSchema(schemaFile);
@@ -78,12 +85,12 @@ public class XSDValidator {
                 LOGGER.error("[XSD validation] fatal error for schema validation, stopping");
             }
         } );
-
     }
 
-    public void validate(String sclFile) {
+    public void validate(String sclData) {
         try {
-            SAXSource source = new SAXSource(new InputSource(new StringReader(sclFile)));
+            prepare(sclData);
+            SAXSource source = new SAXSource(new InputSource(new StringReader(sclData)));
             xsdValidator.validate(source);
         }
         catch(IOException exception) {
@@ -98,5 +105,33 @@ public class XSDValidator {
         var validationError = new ValidationError();
         errorList.add(validationError);
         return validationError;
+    }
+
+    private String getXsdPath(String sclData) {
+        var sclElementAttributes = convertToDocument(sclData)
+                .getElementsByTagName("SCL").item(0).getAttributes();
+
+        var version = getAttributeValue(sclElementAttributes, "version");
+        var revision = getAttributeValue(sclElementAttributes, "revision");
+        var release = getAttributeValue(sclElementAttributes, "release");
+
+        return "/Users/rob/Code/CoMPAS/compas-scl-validator/validator/target/xsd/SCL2007B4/SCL.xsd";
+    }
+
+    private Document convertToDocument(String value) {
+        try {
+            DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder builder = factory.newDocumentBuilder();
+            return builder.parse(new InputSource(new StringReader(value)));
+        } catch (Exception e) {
+            throw new SclValidatorException(CONVERTING_SCL_FILE_ERROR_CODE, "Unable to convert SCL file to document", e);
+        }
+    }
+
+    private String getAttributeValue(NamedNodeMap attributes, String attributeName) {
+        if (attributes.getNamedItem(attributeName) != null) {
+            return attributes.getNamedItem(attributeName).getNodeValue();
+        }
+        return null;
     }
 }
