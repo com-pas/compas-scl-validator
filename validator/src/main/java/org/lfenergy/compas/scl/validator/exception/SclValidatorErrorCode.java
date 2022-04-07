1632f7dc8248c83ed777bb2e1787e43e266b29f2
Dennis Labordus <dennis.labordus@alliander.com>
2022-04-07 07:31:00 +0200

    Added some variant of Async calls.
    Signed-off-by: Dennis Labordus <dennis.labordus@alliander.com>

--- validator/src/main/java/org/lfenergy/compas/scl/validator/exception/SclValidatorErrorCode.java
+++ validator/src/main/java/org/lfenergy/compas/scl/validator/exception/SclValidatorErrorCode.java
@@ -11,6 +11,7 @@ public class SclValidatorErrorCode {
     public static final String NO_SCL_ELEMENT_FOUND_ERROR_CODE = "SVS-0001";
     public static final String LOADING_SCL_FILE_ERROR_CODE = "SVS-0002";
     public static final String LOADING_XSD_FILE_ERROR_CODE = "SVS-0003";
+    public static final String TIMEOUT_ERROR_CODE = "SVS-0004";
 
     public static final String LOADING_OCL_FILES_FAILED = "SVS-1001";
     public static final String LOADING_CUSTOM_OCL_FILES_FAILED = "SVS-1002";
