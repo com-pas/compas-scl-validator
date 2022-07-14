3ccec6661d6325188e8a9b170aa1b720c7f5052a
Dennis Labordus <dennis.labordus@alliander.com>
2022-07-14 13:17:57 +0200

    Fixed most code smells from main branch.
    Signed-off-by: Dennis Labordus <dennis.labordus@alliander.com>

--- riseclipse/validator-riseclipse/src/main/java/org/lfenergy/compas/scl/validator/util/OclUtil.java
+++ riseclipse/validator-riseclipse/src/main/java/org/lfenergy/compas/scl/validator/util/OclUtil.java
@@ -5,17 +5,10 @@ package org.lfenergy.compas.scl.validator.util;
 
 import fr.centralesupelec.edf.riseclipse.iec61850.scl.SclPackage;
 import org.lfenergy.compas.scl.validator.exception.SclValidatorException;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import static org.lfenergy.compas.scl.validator.exception.SclValidatorErrorCode.OCL_MODEL_PACKAGE_NOT_FOUND;
 
 public class OclUtil {
-    private static final Logger LOGGER = LoggerFactory.getLogger(OclUtil.class);
-
-    private static final String FILE_SPECIFICS_DIR_NAME = "FileSpecifics";
-    private static final String COMMON_DIR_NAME = "Common";
-
     OclUtil() {
         throw new UnsupportedOperationException("OclFileUtil class");
     }
